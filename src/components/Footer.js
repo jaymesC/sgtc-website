ed99bad0155c4c785973c4ef6a05ac5f6458fc08
jaymesC <clementjaymes@gmail.com>
2022-01-26 12:48:24 -0800

    chore(): updated many components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,11 @@ import { Link } from "gatsby"
 
 const StyledFooter = styled.section`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   padding-left: 10rem;
   padding-right: 10rem;
   padding-top: 5rem;
-  padding-bottom: 15rem;
+  padding-bottom: 2rem;
   color: #fff;
   background-blend-mode: multiply;
   background-image: radial-gradient(
@@ -29,21 +29,34 @@ const StyledFooter = styled.section`
     padding-left: 1rem;
     padding-right: 1rem;
     padding-top: 2rem;
-    padding-bottom: 5rem;
+    padding-bottom: 2rem;
   }
 
   @media screen and (max-width: 568px) {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    padding-top: 0;
+  }
+`
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 768px) {
+  }
+
+  @media screen and (max-width: 568px) {
+      flex-direction: column;
   }
 `
 
 const StyledH3 = styled.h3`
-  color: #fff;
   font-size: 16px;
   font-weight: 500;
   @media (max-width: 768px) {
   }
 
   @media screen and (max-width: 568px) {
+      margin-top: 2rem;
   }
 `
 
@@ -82,6 +95,7 @@ const StyledIcon = styled.section`
   }
 
   @media screen and (max-width: 568px) {
+    margin-top: 0;
   }
 `
 
@@ -89,9 +103,12 @@ const StyledP = styled.p`
   margin-top: 2rem;
   margin-bottom: 2rem;
   @media (max-width: 768px) {
-  }
+}
 
-  @media screen and (max-width: 568px) {
+@media screen and (max-width: 568px) {
+      margin-top: 1rem;
+      margin-bottom: 1rem;
+      font-size:14px;
   }
 `
 
@@ -107,12 +124,11 @@ const StyledQuickLinks = styled.div`
   }
 `
 const StyledFooterLink = styled(Link)`
-  text-decoration: none;
   color: #fff;
+  text-decoration: none;
   border-bottom: 0.5px solid #f4e8ff;
   padding-bottom: 10px;
-  /* margin-bottom: 2rem;
-  margin-top: 0.1rem; */
+  /* margin-bottom: 2rem;*/
   :hover {
     color: #faec5c;
   }
@@ -120,24 +136,30 @@ const StyledFooterLink = styled(Link)`
   }
 
   @media screen and (max-width: 568px) {
+    margin-top: 1rem;
+    font-size: 14px;
   }
 `
 
 const StyledText = styled.p`
   margin-left: 1rem;
   @media (max-width: 768px) {
-  }
+}
 
-  @media screen and (max-width: 568px) {
+@media screen and (max-width: 568px) {
+      margin-left: 1rem;
+      font-size: 14px;
   }
 `
 
 const StyledFooterContact = styled.div`
   width: 25rem;
   @media (max-width: 768px) {
-  }
+}
 
-  @media screen and (max-width: 568px) {
+@media screen and (max-width: 568px) {
+    width: 20rem;
+    margin-right: 1rem;
   }
 `
 const StyledLocation = styled.div`
@@ -171,60 +193,82 @@ const StyledEmail = styled.div`
 
   @media screen and (max-width: 568px) {
   }
+  `
+
+const TradeMark = styled.p`
+  display: flex;
+  width: 25rem;
+  padding-top: 9rem;
+  align-self: center;
+  color: #fff;
+  @media screen and (max-width: 568px) {
+      width: 20rem;
+      padding-top: 5rem;
+      font-size: 14px;
+  }
 `
 
 
 const Footer = () => {
-  return (
-    <StyledFooter>
-      <StyledAboutSection>
-        <StyledH3>ABOUT SOVEREIGN GRACE TABERNACLE</StyledH3>
-        <StyledP>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro facere
-          aut eius repellendus blanditiis recusandae ipsum. A ad esse provident!
-        </StyledP>
-        <StyledLinks>
-          <StyledIcon>
-            <FaFacebookF />
-          </StyledIcon>
-          <StyledIcon>
-            <FaInstagram />
-          </StyledIcon>
-          <StyledIcon>
-            <FaTwitter />
-          </StyledIcon>
-          <StyledIcon>
-            <FaYoutube />
-          </StyledIcon>
-        </StyledLinks>
-      </StyledAboutSection>
-      <StyledQuickLinks>
-        <StyledH3>QUICK LINKS</StyledH3>
-        <StyledFooterLink to="/">Home</StyledFooterLink>
-        <StyledFooterLink to="/about">About</StyledFooterLink>
-        <StyledFooterLink to="/blog">Blog</StyledFooterLink>
-        <StyledFooterLink to="/contact">Contact</StyledFooterLink>
-      </StyledQuickLinks>
-      <StyledFooterContact>
-        <StyledH3>HAVE A QUESTION?</StyledH3>
-        <StyledLocation>
-          <FaMapMarkerAlt />
-          <StyledText>
-            No. 1b Tony Close Off Okilton Drive, Off Ada George Road, Port
-            Harcourt.
-          </StyledText>
-        </StyledLocation>
-        <StyledNumber>
-          <FaPhoneAlt />
-          <StyledText>+2349075417094</StyledText>
-        </StyledNumber>
+    return (
+      <>
+        <StyledFooter>
+          <StyledContainer>
+            <StyledAboutSection>
+              <StyledH3>ABOUT SOVEREIGN GRACE TABERNACLE</StyledH3>
+              <StyledP>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+                facere aut eius repellendus blanditiis recusandae ipsum. A ad
+                esse provident!
+              </StyledP>
+              <StyledLinks>
+                <StyledIcon>
+                  <FaFacebookF />
+                </StyledIcon>
+                <StyledIcon>
+                  <FaInstagram />
+                </StyledIcon>
+                <StyledIcon>
+                  <FaTwitter />
+                </StyledIcon>
+                <StyledIcon>
+                  <FaYoutube />
+                </StyledIcon>
+              </StyledLinks>
+            </StyledAboutSection>
+            <StyledQuickLinks>
+              <StyledH3>QUICK LINKS</StyledH3>
+              <StyledFooterLink to="/">Home</StyledFooterLink>
+              <StyledFooterLink to="/about">About</StyledFooterLink>
+              <StyledFooterLink to="/blog">Blog</StyledFooterLink>
+              <StyledFooterLink to="/contact">Contact</StyledFooterLink>
+            </StyledQuickLinks>
+            <StyledFooterContact>
+              <StyledH3>HAVE A QUESTION?</StyledH3>
+              <StyledLocation>
+                <FaMapMarkerAlt />
+                <StyledText>
+                  No. 1b Tony Close Off Okilton Drive, Off Ada George Road, Port
+                  Harcourt.
+                </StyledText>
+              </StyledLocation>
+              <StyledNumber>
+                <FaPhoneAlt />
+                <StyledText>+2349075417094</StyledText>
+              </StyledNumber>
               <StyledEmail>
-                  <FaEnvelope />
-                  <StyledText>sovereigngracetabernacle1@gmail.com</StyledText>
-        </StyledEmail>
-      </StyledFooterContact>
-    </StyledFooter>
-  )
+                <FaEnvelope />
+                <StyledText>sovereigngracetabernacle1@gmail.com</StyledText>
+              </StyledEmail>
+            </StyledFooterContact>
+          </StyledContainer>
+          <TradeMark>
+            {" "}
+            Copyright ©{new Date().getFullYear()} All rights reserved | JamesJay
+          </TradeMark>
+        </StyledFooter>
+      </>
+    )
 }
 
 export default Footer
